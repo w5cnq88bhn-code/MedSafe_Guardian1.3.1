a9a406af3428b5d2adf2f8d9de6742907dda66c4
w5cnq88bhn-code <w5cnq88bhn@privaterelay.appleid.com>
2026-04-19 16:31:32 +0800

    Add files via upload

---
+++ medicare-miniprogram/typings/index.d.ts
@@ -0,0 +1,11 @@
+/// <reference types="miniprogram-api-typings" />
+
+interface IAppOption {
+  globalData: {
+    token: string
+    userId: number
+    openid: string
+    currentPatientId: number
+  }
+}
+
